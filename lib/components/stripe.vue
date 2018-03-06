8f82c5fd5264459d33ffc31780509253df2cad92
Mike Sheward <mike@develodesign.co.uk>
2018-03-06 15:40:38 +0000

    Configuring the base package

---
+++ lib/components/stripe.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div>
+        Hello world!
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+  import Vue from 'vue'
+
+  export default {
+    name: 'develodesign-stripe',
+    data () {
+      return {
+      }
+    },
+    components: {
+    }
+  }
+</script>
